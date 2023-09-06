b2a7f38c3077a8e1ce4744cfa7ec199d8c2d41c5
Alexander Drakon <drakon.a.v@gmail.com>
2023-09-06 17:45:22 +0300

    feat: current

--- scripts/kinetics/flat_flame.py
+++ scripts/kinetics/flat_flame.py
@@ -1,46 +1,72 @@
 import cantera as ct
 import numpy as np
+from datetime import datetime
 
 from pathlib import Path
 
-# Inlet Temperature in Kelvin and Inlet Pressure in Pascals
-# In this case we are setting the inlet T and P to room temperature conditions
+from scripts.utils.mixture import get_trifule_for_air
+
+
+
+
+MECHS = {
+    'GRI': 'mechs/GRI/gri30_highT.yaml',
+    'CRECK': 'mechs/CRECK/CRECK_2003_TPRF_HT_LT_ALC_ETHERS.yaml'
+}
+
+
+gas = ct.Solution(MECHS['GRI'])
+
+width = 0.05
+
+
+alphas = [0, 5, 10, 20, 30, 50, 70, 80, 90, 95, 100]
+beta = 0
+
+
+def solve_flame(gas, loglevel=1):
+    f = ct.FreeFlame(gas, width=width)
+    f.set_refine_criteria(ratio=5, slope=0.05, curve=0.1, prune=0.005)
+    f.set_refine_criteria()
+    # Solve with mixture-averaged transport model
+    f.transport_model = 'mixture-averaged'
+    f.solve(loglevel=1, auto=True)
+
+    output = Path() / 'output/temp/adiabatic_flame.yaml'
+    output.unlink(missing_ok=True)
+
+    # Solve with the energy equation enabled
+    f.save(output, name="mix", description="solution with mixture-averaged transport")
+    print(f"mixture-averaged flamespeed = {f.velocity[0]:7f} m/s")
+    # Solve with multi-component transport properties
+    f.transport_model = 'multicomponent'
+    f.solve(loglevel)  # don't use 'auto' on subsequent solves
+    print(f"multicomponent flamespeed = {f.velocity[0]:7f} m/s")
+    f.save(output, name="multi", description="solution with multicomponent transport")
+    # write the velocity, temperature, density, and mole fractions to a CSV file
+    f.save('output/temp/adiabatic_flame.csv', basis="mole", overwrite=True)
+    Su0 = f.velocity[0]
+    print(f"Flame Speed is: {Su0 * 100:.2f} cm/s")
+    return f
+
+
 temperature = 300
-pressure = 1013250
+pressure = ct.one_atm
 
-# Define the gas-mixutre and kinetics
-# In this case, we are choosing a GRI3.0 gas
-gas = ct.Solution('mechs/CRECK/CRECK_2003_TPRF_HT_LT_ALC_ETHERS.yaml')
-# gas = ct.Solution('mechs/GRI/gri30_highT.yaml')
+for alpha in alphas:
+    mixture = get_trifule_for_air('CH4', 'H2', '', alpha, 0)
+    gas.X = mixture
+    gas.TP = temperature, pressure
+    gas.SV = gas.s, gas.v/10
+    print(mixture, gas.P/ct.one_atm, gas.T)
 
-width = 0.03
-
-gas.X = 'CH4:3.5 O2:7 N2:0'
-gas.TP = temperature, pressure
-
-f = ct.FreeFlame(gas, width=width)
-f.set_refine_criteria(ratio=5, slope=0.1, curve=0.2, prune=0.01)
-f.set_refine_criteria()
-
-loglevel = 1
-
-# Solve with mixture-averaged transport model
-f.transport_model = 'mixture-averaged'
-f.solve(loglevel=1, auto=True)
-
-output = Path() / 'output/temp/adiabatic_flame.yaml'
-output.unlink(missing_ok=True)
-
-# Solve with the energy equation enabled
-f.save(output, name="mix", description="solution with mixture-averaged transport")
-print(f"mixture-averaged flamespeed = {f.velocity[0]:7f} m/s")
-# Solve with multi-component transport properties
-f.transport_model = 'multicomponent'
-f.solve(loglevel)  # don't use 'auto' on subsequent solves
-print(f"multicomponent flamespeed = {f.velocity[0]:7f} m/s")
-f.save(output, name="multi", description="solution with multicomponent transport")
-# write the velocity, temperature, density, and mole fractions to a CSV file
-f.save('adiabatic_flame.csv', basis="mole", overwrite=True)
-
-Su0 = f.velocity[0]
-print(f"Flame Speed is: {Su0 * 100:.2f} cm/s")
+with open('output/flame_report.out', 'a') as f:
+    f.write(f'Sequence started at {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
+for alpha in alphas:
+    mixture = get_trifule_for_air('CH4', 'H2', 'CH3OH', alpha, 50)
+    gas.X = mixture
+    gas.TP = temperature, pressure
+    flame = solve_flame(gas, loglevel=1)
+    velocity = flame.velocity[0]
+    with open('output/flame_report.out', 'a') as f:
+        f.write(f'for alpha={alpha} beta={beta} ({mixture}) flame velocity is {velocity*100:.2f} cm/s\n')
--- scripts/utils/mixture.py
+++ scripts/utils/mixture.py
@@ -15,7 +15,6 @@ PERCENT = 100
 o2_fraction = 7
 phi = 1.0
 
-
 def fraction_string(species, fraction):
     return f' {species}:{fraction:.3f}' if fraction > 0 else ''
 
@@ -37,3 +36,31 @@ def get_trifuel_for_o2(o2_fraction, primary, secondary, tertiary, alpha, beta, p
         (diluter, diluter_fraction)
     )]).strip()
     return result
+
+
+def get_trifule_for_air(primary, secondary, tertiary, alpha, beta, phi=1.0):
+    alpha = alpha/100
+    beta = beta/100
+    N2_TO_O2 = 3.77
+    o2_fraction = 1
+    n2_fraction = o2_fraction * N2_TO_O2
+    if not all([species in STOICHS for species in [primary, secondary, tertiary]]):
+        raise IndexError
+    primary_fraction = ((1-alpha) * o2_fraction * STOICHS[primary]/phi) * (1 - beta)
+    secondary_fraction = (alpha * o2_fraction * STOICHS[secondary]/phi) * (1 - beta)
+    tertiary_fraction = o2_fraction * beta * STOICHS[tertiary]/phi
+    total = sum((primary_fraction, secondary_fraction, tertiary_fraction, o2_fraction, n2_fraction))
+    primary_fraction, secondary_fraction, tertiary_fraction, o2_fraction, n2_fraction = [
+        100*fraction/total for fraction in [
+            primary_fraction, secondary_fraction, tertiary_fraction, o2_fraction, n2_fraction
+        ]
+    ]
+    result = ''.join([fraction_string(s, f) for (s, f) in (
+        (primary, primary_fraction),
+        (secondary, secondary_fraction),
+        (tertiary, tertiary_fraction),
+        ('O2', o2_fraction),
+        ('N2', n2_fraction)
+    )]).strip()
+    return result
+
