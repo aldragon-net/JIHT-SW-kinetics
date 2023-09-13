329ca79611fb6f6810981f27bc45785720e1937a
Alexander Drakon <drakon.a.v@gmail.com>
2023-09-13 09:13:53 +0300

    feat: current

--- scripts/kinetics/flat_flame.py
+++ scripts/kinetics/flat_flame.py
@@ -16,7 +16,7 @@ def solve_flame(gas, loglevel=1):
     f = ct.FreeFlame(gas, width=width)
     f.set_refine_criteria(ratio=5, slope=0.08, curve=0.16, prune=0.005)
     f.transport_model = 'mixture-averaged'
-    f.solve(loglevel=1, auto=True)
+    f.solve(loglevel=1)
     output = Path() / 'output/temp/adiabatic_flame.yaml'
     output.unlink(missing_ok=True)
     # Solve with the energy equation enabled
@@ -37,30 +37,31 @@ def solve_flame(gas, loglevel=1):
 temperature = 300
 pressure = ct.one_atm
 compression = 10
-mech_label = 'GRI'
+mech_label = 'CRECK'
 width = 0.05
-alphas = [0, 5, 10, 20, 30, 50, 70, 80, 90, 95, 100]
-beta = 0
+alphas = [0, 10, 20, 30]
+betas = [10, 20, 30, 50, 70]
 
-gas = ct.Solution(MECHS[mech_label], 'gri30')
+gas = ct.Solution(MECHS[mech_label], 'gas')
 
 with open('output/flame/report.out', 'a') as f:
     f.write(f'\nSequence started at {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
     f.write(f'Mechanism is {mech_label}\n')
 for alpha in alphas:
-    mixture = get_trifule_for_air('CH4', 'H2', 'CH3OH', alpha, beta)
-    mixture_label = f'M{alpha}_{beta}'
-    gas.X = mixture
-    gas.TP = temperature, pressure
-    gas.SV = gas.s, gas.v/compression
-    T_comp = gas.T
-    P_comp = gas.P
-    t0 = datetime.now()
-    flame = solve_flame(gas, loglevel=1)
-    t1 = datetime.now()
-    velocity = flame.velocity[0]
-    flame.save(f'output/flame/{mech_label}_{mixture_label}.csv', basis="mole", overwrite=True)
-    with open('output/flame/report.out', 'a') as f:
-        f.write(f'M{alpha}_{beta} ({mixture}) P={P_comp/1e5:.3f} bar T = {T_comp:.0f} K v = {velocity*100:.2f} cm/s (computed in {str(t1 - t0)})\n')
+    for beta in betas:
+        mixture = get_trifule_for_air('CH4', 'H2', 'CH3OCH3', alpha, beta)
+        mixture_label = f'M{alpha}_{beta}(DME)'
+        gas.X = mixture
+        gas.TP = temperature, pressure
+        gas.SV = gas.s, gas.v/compression
+        T_comp = gas.T
+        P_comp = gas.P
+        t0 = datetime.now()
+        flame = solve_flame(gas, loglevel=1)
+        t1 = datetime.now()
+        velocity = flame.velocity[0]
+        flame.save(f'output/flame/{mech_label}_{mixture_label}.csv', basis="mole", overwrite=True)
+        with open('output/flame/report.out', 'a') as f:
+            f.write(f'{mixture_label} ({mixture}) P={P_comp/1e5:.3f} bar T = {T_comp:.0f} K v = {velocity*100:.2f} cm/s (computed in {str(t1 - t0)})\n')
 with open('output/flame/report.out', 'a') as f:
     f.write(f'Sequence completed at {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
